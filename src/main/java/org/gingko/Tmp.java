735c493f5f4695b18c7c72acbeeb3fc4273aa7a5
Kyia <kyia.x52@gmail.com>
2014-03-04 17:52:17 +0800

    1. Some prepare work

---
+++ src/main/java/org/gingko/Tmp.java
@@ -0,0 +1,7 @@
+package org.gingko;
+
+/**
+ * Created by Administrator on 14-3-4.
+ */
+public class Tmp {
+}
---
+++ src/main/java/org/gingko/server/Server.java
@@ -0,0 +1,9 @@
+package org.gingko.server;
+
+/**
+ * Created by Administrator on 14-3-4.
+ */
+public interface Server {
+
+
+}
